12932c0b59a1d0b384ac0ef238209071b07ed9c0
Andrew Glaude <ajgajg1134@gmail.com>
2026-04-18 14:06:16 -0400

    init commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ajgajg1134/televid
+
+go 1.26.2
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/philhofer/tcx v0.0.0-20140422172628-84b8274e31dc // indirect
+	github.com/philhofer/vec v0.0.0-20140421144027-536fc796d369 // indirect
+	golang.org/x/image v0.39.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"math"
+	"sync"
+
+	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"github.com/philhofer/tcx"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/gomonobold"
+)
+
+// 1 m/s = 2.2369 mph
+const MPS_CONVERT = 2.2369
+
+func main() {
+	activities, err := tcx.ReadFile("Harvard_Road_Race.tcx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	minLat, maxLat, minLong, maxLong := activities.Acts.Act[0].Laps[0].Trk.Pt[0].Lat, activities.Acts.Act[0].Laps[0].Trk.Pt[0].Lat, activities.Acts.Act[0].Laps[0].Trk.Pt[0].Long, activities.Acts.Act[0].Laps[0].Trk.Pt[0].Long
+
+	coords := []coord{}
+
+	for _, activity := range activities.Acts.Act {
+		for _, lap := range activity.Laps {
+			for _, trackpoint := range lap.Trk.Pt {
+				minLat = math.Min(minLat, trackpoint.Lat)
+				minLong = math.Min(minLat, trackpoint.Long)
+
+				maxLat = math.Max(maxLat, trackpoint.Lat)
+				maxLong = math.Max(maxLong, trackpoint.Long)
+				coords = append(coords, coord{
+					Lat:  trackpoint.Lat,
+					Long: trackpoint.Long,
+				})
+			}
+		}
+	}
+
+	fmt.Printf("%10.10f %10f %10f %10f\n", minLat, maxLat, minLong, maxLong)
+
+	miniMap := drawMap(coords)
+
+	fts := []frameTelemetry{}
+
+	for _, activity := range activities.Acts.Act {
+		for _, lap := range activity.Laps {
+			limit := 200
+			for i := range len(lap.Trk.Pt) - 1 {
+				pt := lap.Trk.Pt[i]
+				ptNext := lap.Trk.Pt[i+1]
+				if i >= limit {
+					break
+				}
+				fts = append(fts, frameTelemetry{
+					Power: pt.Power,
+					Speed: pt.Speed,
+					HR:    pt.HR,
+					Lat:   pt.Lat,
+					Long:  pt.Long,
+				})
+				fts = append(fts, frameTelemetry{
+					Power: infill(pt.Power, ptNext.Power, 0.8),
+					Speed: infill(pt.Speed, ptNext.Speed, 0.8),
+					HR:    infill(pt.HR, ptNext.HR, 0.8),
+					Lat:   infill(pt.Lat, ptNext.Lat, 0.8),
+					Long:  infill(pt.Long, ptNext.Long, 0.8),
+				})
+				fts = append(fts, frameTelemetry{
+					Power: infill(pt.Power, ptNext.Power, 0.6),
+					Speed: infill(pt.Speed, ptNext.Speed, 0.6),
+					HR:    infill(pt.HR, ptNext.HR, 0.6),
+					Lat:   infill(pt.Lat, ptNext.Lat, 0.6),
+					Long:  infill(pt.Long, ptNext.Long, 0.6),
+				})
+				fts = append(fts, frameTelemetry{
+					Power: infill(pt.Power, ptNext.Power, 0.4),
+					Speed: infill(pt.Speed, ptNext.Speed, 0.4),
+					HR:    infill(pt.HR, ptNext.HR, 0.4),
+					Lat:   infill(pt.Lat, ptNext.Lat, 0.4),
+					Long:  infill(pt.Long, ptNext.Long, 0.4),
+				})
+				fts = append(fts, frameTelemetry{
+					Power: infill(pt.Power, ptNext.Power, 0.2),
+					Speed: infill(pt.Speed, ptNext.Speed, 0.2),
+					HR:    infill(pt.HR, ptNext.HR, 0.2),
+					Lat:   infill(pt.Lat, ptNext.Lat, 0.2),
+					Long:  infill(pt.Long, ptNext.Long, 0.2),
+				})
+			}
+		}
+	}
+
+	work := make(chan *frameWork)
+	wg := sync.WaitGroup{}
+	for range 8 {
+		wg.Go(func() {
+			font, err := truetype.Parse(gomonobold.TTF)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			bigFace := truetype.NewFace(font, &truetype.Options{Size: 75})
+			smallFace := truetype.NewFace(font, &truetype.Options{Size: 55})
+			for ft := range work {
+				generateImage(ft.ft, ft.i, miniMap, bigFace, smallFace)
+			}
+		})
+	}
+
+	for i, ft := range fts {
+		if i%10 == 0 {
+			fmt.Printf("%d / %d %0.2f%%\n", i, len(fts), float64(i)/float64(len(fts))*100.0)
+		}
+		work <- &frameWork{ft: &ft, i: i}
+	}
+	close(work)
+	wg.Wait()
+	fmt.Println("Done.")
+}
+
+func infill(a, b float64, ratA float64) float64 {
+	return (a * ratA) + (b * (1 - ratA))
+}
+
+type frameWork struct {
+	ft *frameTelemetry
+	i  int
+}
+
+type frameTelemetry struct {
+	Power float64
+	Speed float64
+	HR    float64
+	Lat   float64
+	Long  float64
+}
+
+func generateImage(frameTelemetry *frameTelemetry, i int, staticImg image.Image, bigFace, smallFace font.Face) {
+	ggCtx := gg.NewContext(1920, 1080)
+	ggCtx.SetRGB(0, 1, 0)
+	ggCtx.Clear()
+	ggCtx.DrawImage(staticImg, 0, 0) // TODO: use contexts smarter to make this faster?
+	//Big Fonts
+	ggCtx.SetFontFace(bigFace)
+	drawStringDropShadow(ggCtx, fmt.Sprintf("%3.0f mph", frameTelemetry.Speed*MPS_CONVERT), 20, 95)
+
+	ggCtx.SetFontFace(smallFace)
+	drawStringDropShadow(ggCtx, fmt.Sprintf("%04.0f w", frameTelemetry.Power), 55, 1050)
+	drawWattBox(ggCtx, frameTelemetry.Power)
+
+	drawStringDropShadow(ggCtx, fmt.Sprintf("%3.f bpm", frameTelemetry.HR), 395, 1050)
+	drawHRBox(ggCtx, frameTelemetry.HR)
+
+	ggCtx.SetRGBA(0.2, 0.2, 0.7, 1) // GPS point color
+	gx, gy := scaleGPS(frameTelemetry.Lat, frameTelemetry.Long)
+	ggCtx.DrawCircle(gy, gx, 8)
+	ggCtx.Fill()
+
+	ggCtx.SavePNG(fmt.Sprintf("imgs/%04d.png", i))
+}
+
+func drawWattBox(ggCtx *gg.Context, watts float64) {
+	height, width, x, y := 50., 270., 55., 950.
+	grad := gg.NewLinearGradient(x, y, x+width, y+height)
+
+	// https://uigradients.com/#KingYna
+	grad.AddColorStop(0, color.RGBA{R: 26, G: 42, B: 252, A: 255})
+	grad.AddColorStop(0.5, color.RGBA{R: 178, G: 31, B: 0x1f, A: 255})
+	grad.AddColorStop(1, color.RGBA{R: 0xfd, G: 0xbb, B: 0x2d, A: 255})
+
+	ggCtx.SetLineWidth(4)
+	ggCtx.SetLineCapRound()
+	ggCtx.SetRGB(0.1, 0.1, 0.15)
+	ggCtx.DrawRectangle(x, y, width, height)
+	ggCtx.Stroke()
+
+	//Scale width. We want 30 -> 300
+	calcWidth := watts / 450. * 250.
+	calcWidth = math.Min(width, calcWidth+30)
+
+	ggCtx.SetFillStyle(grad)
+	ggCtx.DrawRectangle(x, y, calcWidth, height)
+
+	ggCtx.Fill()
+}
+
+func drawHRBox(ggCtx *gg.Context, bpm float64) {
+	height, width, x, y := 50., 270., 395., 950.
+	grad := gg.NewLinearGradient(x, y, x+width, y+height)
+
+	// https://uigradients.com/#Kyoto
+	grad.AddColorStop(0, color.RGBA{R: 0xc2, G: 0x15, B: 0x0, A: 255})
+	grad.AddColorStop(1, color.RGBA{R: 0xff, G: 0xc5, B: 0x0, A: 255})
+
+	ggCtx.SetLineWidth(4)
+	ggCtx.SetLineCapRound()
+	ggCtx.SetRGB(0.15, 0.08, 0.08)
+	ggCtx.DrawRectangle(x, y, width, height)
+	ggCtx.Stroke()
+
+	//Scale width. We want 30 -> 300
+	calcWidth := bpm / 200. * 180.
+	calcWidth = math.Min(width, calcWidth+30)
+
+	ggCtx.SetFillStyle(grad)
+	ggCtx.DrawRectangle(x, y, calcWidth, height)
+
+	ggCtx.Fill()
+}
+
+func drawStringDropShadow(ggCtx *gg.Context, s string, x, y float64) {
+	shadowDepth := 4.
+	// Shadow color
+	ggCtx.SetRGBA(0, 0, 0, 1)
+	ggCtx.DrawString(s, x+shadowDepth, y+shadowDepth)
+	//Text color
+	ggCtx.SetRGBA(0.9, 0.9, 0.9, 1)
+	ggCtx.DrawString(s, x, y)
+}
+
+func scaleGPS(lat float64, long float64) (float64, float64) {
+	// technically this should probably use minLat and minLong and the scale factor is 1 / (max - min)
+	scaleFactor := 3_546.1 * 15
+	lat = lat - 42.291441
+	lat = lat * scaleFactor * -1
+
+	//x = x / -42.291723
+	long = long + 71.196479
+	//y = y / 71.196104
+	long = long * scaleFactor
+
+	return lat + 1050, long + 1850
+}
+
+type coord struct {
+	Lat  float64
+	Long float64
+}
+
+func drawMap(coords []coord) image.Image {
+	ggCtx := gg.NewContext(1920, 1080)
+	ggCtx.SetRGBA(0, 0, 0, 0)
+	ggCtx.Clear()
+	//minimap color
+	ggCtx.SetRGBA(0.1, 0.1, 0.2, 1)
+
+	for _, coord := range coords {
+		gx, gy := scaleGPS(coord.Lat, coord.Long)
+		// fmt.Printf("drawing point at %f, %f\n", gx, gy)
+		ggCtx.DrawCircle(gy, gx, 5)
+		ggCtx.Fill()
+	}
+
+	return ggCtx.Image()
+}
+
+// Want 42.291441 to be 0
+// want 42.291723 to be 1
